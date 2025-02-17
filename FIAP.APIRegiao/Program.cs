ee2639c10d66c73c2223f3dabd7c72905edeac5a
bellakratchei <isabellavk@hotmail.com.br>
2025-02-17 12:30:16 -0300

    Configurado inicialmente prometheus na API de Regiao

--- FIAP.APIRegiao/Program.cs
+++ FIAP.APIRegiao/Program.cs
@@ -2,6 +2,7 @@ using FIAP.APIRegiao.Events;
 using FIAP.APIRegiao.Repository;
 using FIAP.APIRegiao.Service;
 using Microsoft.EntityFrameworkCore;
+using Prometheus;
 
 namespace FIAP.APIRegiao
 {
@@ -86,7 +87,11 @@ namespace FIAP.APIRegiao
             app.UseCors("AllowAll");
             app.UseHttpsRedirection();
             app.UseAuthorization();
+
+            // Middleware de métricas do Prometheus
+            app.UseHttpMetrics(); // Captura métricas HTTP automaticamente
             app.MapControllers();
+            app.MapMetrics(); // Expondo métricas no endpoint /metrics
 
             app.Run();
         }
